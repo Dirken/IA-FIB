08194c45e9bb50ab1069049473147a6a1a5e078b
Alex Cuello ortiz <acuellor.6@gmail.com>
2016-10-17 16:36:23 +0200

    Lo que hicimos en clase

--- Azamon/src/azamon/Azamon.java
+++ Azamon/src/azamon/Azamon.java
@@ -5,6 +5,7 @@
  */
 package azamon;
 import IA.Azamon.*;
+import java.util.Scanner; 
 
 /**
  *
@@ -17,7 +18,26 @@ public class Azamon {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Oferta oferta = new Oferta(12, 12, 12);
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        
+        //System.out.println("Semilla: ");
+        int semilla = 4; //reader.nextInt();
+        
+        //System.out.println("Número de paquetes: ");
+        int numeroPaquetes = 100; //reader.nextInt(); // Scans the next token of the input as an int.
+        Paquetes paquetes = new Paquetes(numeroPaquetes,semilla);
+        
+        //System.out.println("Número de paquetes: ");
+        double proporcion = 1.2;
+        Transporte transporte = new Transporte(paquetes, proporcion , semilla);
+        System.out.println(transporte);
+        
+        
+        
+        
+        
+        
+        System.out.println(paquetes);
     }
     
 }
