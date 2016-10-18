5341095835640619983a18662f59dc05e5a53677
RMEYERHOFER\Ricard <ricardmeyerhofer@gmail.com>
2016-10-18 16:58:52 +0200

    Clases añadidas métodos generadores y estado final

---
+++ Azamon/src/azamon/GeneradorHillClimbing.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package azamon;
+
+/**
+ *
+ * @author Ricard
+ */
+public class GeneradorHillClimbing {
+    /*
+Para Hill Climbing tendréis que generar todas las posibles aplicaciones de los operadores al
+estado actual
+    */
+    
+}
---
+++ Azamon/src/azamon/GeneradorSimulatedAnneiling.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package azamon;
+
+/**
+ *
+ * @author Ricard
+ */
+public class GeneradorSimulatedAnneiling {
+    /*
+    Para Simulated Annealing tendréis que escoger al azar un operador y generar
+solo un sucesor aplicando este operador con parámetros también al azar.
+    */
+    
+}
