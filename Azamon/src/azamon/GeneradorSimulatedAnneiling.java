2924861c13b4b6539a6e68ed1d0a5a6e1acf1d22
RMEYERHOFER\Ricard <ricardmeyerhofer@gmail.com>
2016-10-19 15:18:02 +0200

    no message

--- Azamon/src/azamon/GeneradorSimulatedAnneiling.java
+++ Azamon/src/azamon/GeneradorSimulatedAnneiling.java
@@ -5,14 +5,46 @@
  */
 package azamon;
 
+import IA.Azamon.Oferta;
+import IA.Azamon.Paquete;
+import IA.Azamon.Transporte;
+import java.util.Random;
+
 /**
  *
  * @author Ricard
  */
-public class GeneradorSimulatedAnneiling {
-    /*
-    Para Simulated Annealing tendréis que escoger al azar un operador y generar
+
+/*
+Para Simulated Annealing tendréis que escoger al azar un operador y generar
 solo un sucesor aplicando este operador con parámetros también al azar.
-    */
+*/    
+public class GeneradorSimulatedAnneiling {
+
+    private Random random;
+    
+    public GeneradorSimulatedAnneiling(int semilla) {
+        random = new Random(semilla);
+        if (random.nextInt(2) == 0) {
+            Paquete paquete = null;
+            Oferta oferta = null;
+            mover(paquete, oferta);
+        } 
+        else {
+            Paquete paquete1 = null;
+            Paquete paquete2 = null;
+            Oferta oferta = null;
+            intercambiar(paquete1, paquete2, oferta);
+        }
+    }
+
+    private void mover(Paquete paquete, Oferta oferta) {
+        
+        
+    }
+
+    private void intercambiar(Paquete paquete1, Paquete paquete2, Oferta oferta) {
+        
+    }
     
 }
