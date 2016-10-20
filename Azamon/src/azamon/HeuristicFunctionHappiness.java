0063816f331d1c57d6b55d84390d5de2336e20cc
PC-DAVID\dracu <david.marin.info@gmail.com>
2016-10-20 15:41:41 +0200

    Heuristics final

---
+++ Azamon/src/azamon/HeuristicFunctionHappiness.java
@@ -0,0 +1,16 @@
+package azamon;
+import aima.search.framework.HeuristicFunction;
+
+public class HeuristicFunctionHappiness implements HeuristicFunction{
+    
+    /**
+     * Funcion que retorna el heurístico por coste de un estado.
+     * Sobreescribimos la clase "HeuristicFunction" del aima.
+     * @param state Nuevo estado del que calcular el heurístico
+     * @return Heurístico por coste del estado.
+     */
+    @Override
+    public double getHeuristicValue(Object state) {
+        return ((Estado)state).getHappiness();
+    }
+}
